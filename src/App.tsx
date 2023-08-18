b716d01a04fa7ea9d672b04fbed3d6474ef84415
Vlad <Smirnov.ru92@mail.ru>
2023-08-18 20:19:12 +0500

    add getState functional

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import styles from "./App.module.scss";
 import {observer} from "mobx-react";
 import {SearchForm} from "../src/components/SearchForm";
 import {AddInstrument} from "../src/components/AddInstrument";
+import GetState from "../src/components/getState";
+import {Store} from "../src/store";
 
 const App = observer(() => {
     return (
         <div className={styles.App}>
-           <h1> Minicut Craft Pro v0.05 </h1>
+           <h1> Minicut Craft Pro v0.06 </h1>
             <SearchForm/>
             <AddInstrument/>
+            <GetState store={Store}/>
         </div>
     );
 });
---
+++ src/components/getState.tsx
@@ -0,0 +1,19 @@
+// import "./styles.css";
+import { Highlighter } from "rc-highlight";
+import { observer } from "mobx-react";
+import { Store } from "../store";
+import {toJS} from "mobx";
+
+const GetState = observer((props: { store: any }) => {
+    // debugger
+
+    const res = toJS(props.store.dataArray)
+    console.log(res)
+    return (
+        <div style={{ maxWidth: '80%' }}>
+            <Highlighter  >{`${JSON.stringify(res)}`}</Highlighter>
+        </div>
+    );
+});
+
+export default GetState;
